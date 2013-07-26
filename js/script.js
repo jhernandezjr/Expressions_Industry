f549c01595d146c99983216c99607b30fd84fc37
Jose.Hernandez <joey.hernandezjr@gmail.com>
2013-07-25 23:33:05 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var computer = prompt("Do you own a computer")
+
+var software = prompt("Do you have Adobe CS6")
+
+if (computer = yes){
+	document.write("You are done with the first step")
+}
+if (computer = no){
+	document.write("You need to go buy one")
+}
+
+
+
+if (software = yes){
+	document.write("You are set to go")
+}
+if (software = no){
+	document.write("You need to go buy it")
+}
+	
